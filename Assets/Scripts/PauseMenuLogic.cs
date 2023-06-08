e897b49e73771a56898d5f9da4dc8ad20e8edb9e
Ilya <thirdilno@gmail.com>
2023-06-08 21:07:44 +0700

    pause menu done, esc handling to be fixed

---
+++ Assets/Scripts/PauseMenuLogic.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuLogic : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused =false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            if (!isPaused)
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
